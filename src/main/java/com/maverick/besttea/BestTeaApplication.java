e56762f2fdca317303e39a0c6d6e980588bdf197
ganesh <gpagar641@gmail.com>
2018-10-03 22:14:26 +0530

    //Create new Project  :-  Ganesh

---
+++ src/main/java/com/maverick/besttea/BestTeaApplication.java
@@ -0,0 +1,12 @@
+package com.maverick.besttea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestTeaApplication.class, args);
+	}
+}
